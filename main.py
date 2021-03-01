fac259ecf65ddc5389bc69cc4edaeadd97126d66
nikitaz <nikita.zinchenko.2000@mail.ru>
2021-03-02 00:09:16 +0200

    added one more function for calculating mines and added game functionality for some functions

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
-def createGame():
+import random
+
+N, M = (5, 10) # N -> playing field size NxN, M -> numbers of mines
+
+
+def getTotalMines(PM, i, j) -> int:
+    n = 0
+    for k in range(-1, 2):
+        for l in range(-1, 2):
+            x = i + k
+            y = j + l
+            if x < 0 or x >= N or y < 0 or y >= N:
+                continue
+            if PM[x*N+y] < 0:
+                n += 1
+    return n
+
+def createGame(PM):
     """Creation of the playing field: the location of mines
        and counting the number of mines around the cells without mines
     """
-    pass
+
+    # if the cell has the value n>=0, then there are no mines in it
+    # and the number shows the number of mines around the current cell.
+    # But all minus numbers are mines
 
 
-def show():
+    rng = random.Random()
+    n = M
+    while n > 0:
+        i = rng.randrange(N) # random num from 0 to N
+        j = rng.randrange(N)
+        if PM[i*N+j] != 0: # if this field has already opened
+            continue
+        PM[i*N+j] = -1 # а иначе запишем туда значение -1
+        n -= 1
+
+    # выяесляем количество мин вокруг клетки
+    # (это число будет показоваться игроку при открытии ячейки)
+    for i in range(N):
+        for j in range(N):
+            # если текущая открытая клетка >= 0 то мы присваиваем функцию,
+            # которая высчитывает количество мин рядом с этой клеткой
+            if PM[i*N+j] == 0:
+                PM[i*N+j] = getTotalMines(PM, i, j)
+
+
+def show(pole):
     """The function displays the state of the current
        playing field
     """
-    pass
+    print("Pole PM: ")
+    for i in range(N):
+        for j in range(N):
+            print(str(pole[i*N+j]).rjust(3), end="")
+        print()
 
 
 def goPlayer():
@@ -31,6 +75,11 @@ def startGame():
        the player opens any closed cell, the result is checked
        for the presence of a mine or a winning situation
     """
+    # P nad PM - it's a playing fields
+    P = [-2]*N*N
+    PM = [0]*N*N
+    createGame(PM)
+    show(PM)
     while isFinish():
         show()
         goPlayer()
